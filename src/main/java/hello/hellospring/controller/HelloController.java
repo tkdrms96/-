d490f0cfe9444f1a62fe41d7fcc57595c1aba07b
a18ra <a18rama@naver.com>
2022-07-21 21:50:33 +0900

    view 기기기기기초

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,4 +14,40 @@ public class HelloController {
         model.addAttribute("data","hello!!");
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
+        /*requestParam의 required default가 true임 required true 일 경우 무조건 url에 파라미터이 있어야함 아닐경우 에러*/
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody /*default로 제이슨으로 반환 해줌*/
+    /*객체(키 값)로 들어왔을때, json 객체로 만들어서 요청한 웹문서에 그대로 응답시켜줌 JsonConverter*/
+    /*StringConverter는 객체로 들어오지 않았을경우 문자로만 반환시켜줌*/
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
